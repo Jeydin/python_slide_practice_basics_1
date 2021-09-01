8e83665beca03ecf299f9d720bb0ed3bc5ebcaf4
Jeydin Pham <89167700+Jeydin@users.noreply.github.com>
2021-09-01 16:28:15 +0000

    Added Hello World

--- slide_practice.py
+++ slide_practice.py
@@ -3,3 +3,12 @@
 # Python Basics Slide Practice (Notes)
 
 print("This is our slide_practice file.")
+
+# Blank line
+print()
+
+
+print("This is line 11.")
+
+# Changing the end
+print("Hello, World", end = " === ")
